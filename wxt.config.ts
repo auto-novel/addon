20810165da4ec78302bf1c6295af2f1c6fd972ca
FishHawk <whang23@outlook.com>
2025-10-10 23:52:29 +0800

    firefox mv3不支持调试，另外以后可能出firefox mv2版本

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,51 +6,47 @@ export default defineConfig({
   imports: {
     eslintrc: { enabled: true },
   },
-  manifest: ({ browser, manifestVersion, mode, command }) => {
-    if (manifestVersion != 3) throw new Error("只支持 Manifest V3");
-
-    return {
-      name: "轻小说机翻机器人",
-      homepage_url: "https://n.novelia.cc/",
-      action: {
-        default_title: "轻小说机翻站用户侧爬虫插件",
-      },
-      externally_connectable: {
-        matches: [
-          "*://*.novelia.cc/*",
-          "*://*.fishhawk.top/*",
-          "*://localhost/*",
-          "*://example.com/*",
-        ],
-      },
-      permissions: [
-        "tabs",
-        "scripting",
-        "alarms",
-        "cookies",
-        "activeTab",
-        "storage",
-        "webRequest",
-        "declarativeNetRequest",
-        "declarativeNetRequestWithHostAccess",
+  manifest: {
+    name: "轻小说机翻机器人",
+    homepage_url: "https://n.novelia.cc/",
+    action: {
+      default_title: "轻小说机翻站用户侧爬虫插件",
+    },
+    externally_connectable: {
+      matches: [
+        "*://*.novelia.cc/*",
+        "*://*.fishhawk.top/*",
+        "*://localhost/*",
+        "*://example.com/*",
       ],
-      host_permissions: [
-        "https://*.novelia.cc/*",
-        "https://*.fishhawk.top/*",
+    },
+    permissions: [
+      "tabs",
+      "scripting",
+      "alarms",
+      "cookies",
+      "activeTab",
+      "storage",
+      "webRequest",
+      "declarativeNetRequest",
+      "declarativeNetRequestWithHostAccess",
+    ],
+    host_permissions: [
+      "https://*.novelia.cc/*",
+      "https://*.fishhawk.top/*",
 
-        "https://*.baidu.com/*",
-        "https://*.youdao.com/*",
-        "https://*.openai.com/*",
-        "https://*.amazon.co.jp/*",
-        "https://kakuyomu.jp/*",
-        "https://*.syosetu.com/*",
-        "https://novelup.plus/*",
-        "https://syosetu.org/*",
-        "https://*.pixiv.net/*",
-        "https://*.alphapolis.co.jp/*",
-        "https://novelism.jp/*",
-      ],
-    };
+      "https://*.baidu.com/*",
+      "https://*.youdao.com/*",
+      "https://*.openai.com/*",
+      "https://*.amazon.co.jp/*",
+      "https://kakuyomu.jp/*",
+      "https://*.syosetu.com/*",
+      "https://novelup.plus/*",
+      "https://syosetu.org/*",
+      "https://*.pixiv.net/*",
+      "https://*.alphapolis.co.jp/*",
+      "https://novelism.jp/*",
+    ],
   },
   srcDir: "src",
   webExt: {

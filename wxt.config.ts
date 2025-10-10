0f2639ac7e94c04b823538c0ec8a741f3783f888
FishHawk <whang23@outlook.com>
2025-10-11 00:39:52 +0800

    调整wxt config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "wxt";
+import { defineConfig, UserManifest } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -6,10 +6,9 @@ export default defineConfig({
   imports: {
     eslintrc: { enabled: true },
   },
-  manifest: ({ browser, manifestVersion, mode, command }) => {
-    if (manifestVersion != 3) throw new Error("只支持 Manifest V3");
-
-    const baseManifest = {
+  manifestVersion: 3,
+  manifest: ({ browser }) => {
+    const userManifest: UserManifest = {
       name: "轻小说机翻机器人",
       homepage_url: "https://n.novelia.cc/",
       action: {
@@ -62,22 +61,18 @@ export default defineConfig({
     };
 
     if (browser === "chrome") {
-      return {
-        ...baseManifest,
-        key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmzpvkqphjS1Od8waHDo12FnmcE7+QkzVTr/MKtv64UzlDhF54W8nJR2v4qWy+gHzJkf6QakEELfE4jOJAdOPxNQFd8YgEvsYk8Acfo9Pyki/3jpBB8dzDemAjABNasNGyd9RIHdOuZd2Evl+0NyUNHaCSzSgPJBcGZzg8ACfp8VGhPltiNlxy/JXQJZStR35fiFndC3WP9qaeztLy+jeg5ieQY8ULNLgJDbL02S1KEIB0ijaR9CICaiSYsPFdPhMlYdR8kYhHhrRfXljoJPw4LH0ThMrOoeNWHI6DgO/qSXgSDx4LknOJ28UPCEqHTkPo6WTDDNLpDhFG3ZwMbpHmQIDAQAB",
-      };
+      userManifest.key =
+        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmzpvkqphjS1Od8waHDo12FnmcE7+QkzVTr/MKtv64UzlDhF54W8nJR2v4qWy+gHzJkf6QakEELfE4jOJAdOPxNQFd8YgEvsYk8Acfo9Pyki/3jpBB8dzDemAjABNasNGyd9RIHdOuZd2Evl+0NyUNHaCSzSgPJBcGZzg8ACfp8VGhPltiNlxy/JXQJZStR35fiFndC3WP9qaeztLy+jeg5ieQY8ULNLgJDbL02S1KEIB0ijaR9CICaiSYsPFdPhMlYdR8kYhHhrRfXljoJPw4LH0ThMrOoeNWHI6DgO/qSXgSDx4LknOJ28UPCEqHTkPo6WTDDNLpDhFG3ZwMbpHmQIDAQAB";
     } else if (browser === "firefox") {
-      return {
-        ...baseManifest,
-        browser_specific_settings: {
-          gecko: {
-            id: "addon@n.novelia.cc",
-          },
+      userManifest.browser_specific_settings = {
+        gecko: {
+          id: "addon@n.novelia.cc",
         },
-      };
+      }
     } else {
       throw new Error(`不支持的浏览器类型: ${browser}`);
     }
+    return userManifest;
   },
   srcDir: "src",
   webExt: {

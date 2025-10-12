ef49d300c7aeea8fe93df7a4762472745aebf9b1
FishHawk <whang23@outlook.com>
2025-10-12 22:41:14 +0800

    chore: 整理content脚本

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,22 +1,25 @@
-import { browserInfo, IS_DEBUG } from "@/utils/consts";
 import { MessageType } from "@/rpc/types";
 
-/*
-  Here we use externally_connectable in manifest.json to allow the web page to connect to the extension directly.
-*/
-function process_forward(event: MessageEvent) {
-  if (browserInfo.isChrome) {
-    browser.runtime.sendMessage(event.data, (response) => {
-      debugLog("Crawler response:", response);
-      window.postMessage(response, event.origin);
-    });
-  } else if (browserInfo.isFirefox) {
+function forwardMessageToAddon() {
+  window.addEventListener("message", (event) => {
+    if (event.source !== window) {
+      return;
+    }
+
+    let isValidMessage = false;
+    for (const key of Object.keys(MessageType)) {
+      if (event.data?.type === (MessageType as any)[key]) {
+        isValidMessage = true;
+        break;
+      }
+    }
+    if (event.data?.type == MessageType.Response) return; // Ignore responses.
+    if (!isValidMessage) return; // Ignore unknown messages.
+
     browser.runtime.sendMessage(event.data).then((resp) => {
       window.postMessage(resp, event.origin);
     });
-  } else {
-    throw new Error("Unsupported browser");
-  }
+  });
 }
 
 export default defineContentScript({
@@ -25,31 +28,15 @@ export default defineContentScript({
     "https://*.novelia.cc/*",
     "https://*.fishhawk.top/*",
   ],
-  main() {
+  async main() {
     debugLog("Content script for auto-novel loaded.");
-    window.addEventListener("message", (event) => {
-      if (event.source !== window) {
-        return;
-      }
+    if (import.meta.env.FIREFOX) {
+      forwardMessageToAddon();
+    }
 
-      let isValidMessage = false;
-      for (const key of Object.keys(MessageType)) {
-        if (event.data?.type === (MessageType as any)[key]) {
-          if (browserInfo.isChrome) {
-            if (IS_DEBUG) {
-              debugLog(
-                "In Production Mode, please use chrome.runtime.postMessage",
-              );
-            }
-            return;
-          }
-          isValidMessage = true;
-          break;
-        }
-      }
-      if (event.data?.type == MessageType.Response) return; // Ignore responses.
-      if (!isValidMessage) return; // Ignore unknown messages.
-      process_forward(event);
+    debugLog("Injecting Addon into web page.");
+    await injectScript("/addon-world.js", {
+      keepInDom: true,
     });
   },
 });

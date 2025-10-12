571f81ae7716c5389bd0dc387d6c7386b60024fb
FishHawk <whang23@outlook.com>
2025-10-12 23:05:01 +0800

    fix: 修复IS_DEBUG

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
-export const IS_DEBUG = import.meta.env.PROD;
+export const IS_DEBUG = import.meta.env.DEV;
 
 export const MAX_PAGE_LOAD_WAIT_TIME = 10000; // ms
 export const DELAYED_TAB_CLOSE_TIME = 3000; // ms

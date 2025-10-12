2d000f5c1987050db83395bf6584780515450a81
FishHawk <whang23@outlook.com>
2025-10-12 21:39:06 +0800

    更新选项页面

--- src/entrypoints/options/App.vue
+++ src/entrypoints/options/App.vue
@@ -1,54 +1,26 @@
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
-import * as Api from "@/utils/api";
 import Case from "./components/Case.vue";
 import Divider from "./components/Divider.vue";
-
-interface TestCase {
-  name: string;
-  status: "executing" | "success" | "error" | null;
-  test: () => Promise<boolean>;
-}
-
-function newHttpFetchCase(url: string, content: string) {
-  return {
-    name: `http_fetch ${url}`,
-    status: null,
-    test: async () => {
-      const resp = await Api.http_fetch(url, {});
-      return resp.status === 200 && resp.body.includes(content);
-    },
-  } as TestCase;
-}
+import type { TestCase } from "./test";
+import { Test } from "./test";
 
 const cases = ref<TestCase[]>([
-  newHttpFetchCase(
+  Test.httpFetchCase(
     "https://www.amazon.co.jp/dp/4098505789",
     "異世界転生して魔女になったの",
   ),
-  newHttpFetchCase(
+  Test.tabHttpFetchCase(
+    "https://www.amazon.co.jp/",
     "https://www.amazon.co.jp/dp/4098505789",
     "異世界転生して魔女になったの",
   ),
 ]);
 
-async function runTestCase(testCase: TestCase) {
-  if (testCase.status === "executing") return;
-  testCase.status = "executing";
-  await testCase
-    .test()
-    .then((result) => {
-      testCase.status = result ? "success" : "error";
-    })
-    .catch(() => {
-      testCase.status = "error";
-    });
-}
-
 onMounted(async () => {
   for (const c of cases.value) {
-    await runTestCase(c);
+    await Test.runTestCase(c);
   }
 });
 </script>
@@ -58,7 +30,7 @@ onMounted(async () => {
     <h1 class="text-3xl font-bold text-gray-900 my-12">测试用例</h1>
     <Divider />
     <template v-for="c in cases" :key="c.name">
-      <Case :name="c.name" :status="c.status" @run="runTestCase(c)" />
+      <Case :name="c.name" :status="c.status" @run="Test.runTestCase(c)" />
       <Divider />
     </template>
   </div>
---
+++ src/entrypoints/options/test.ts
@@ -0,0 +1,48 @@
+import * as Api from "@/utils/api";
+
+export interface TestCase {
+  name: string;
+  status: "executing" | "success" | "error" | null;
+  test: () => Promise<boolean>;
+}
+
+function httpFetchCase(url: string, content: string) {
+  return {
+    name: `http_fetch ${url}`,
+    status: null,
+    test: async () => {
+      const resp = await Api.http_fetch(url, {});
+      return resp.status === 200 && resp.body.includes(content);
+    },
+  } as TestCase;
+}
+
+function tabHttpFetchCase(tabUrl: string, url: string, content: string) {
+  return {
+    name: `tab_http_fetch ${url}`,
+    status: null,
+    test: async () => {
+      const resp = await Api.tab_http_fetch(tabUrl, url);
+      return resp.status === 200 && resp.body.includes(content);
+    },
+  } as TestCase;
+}
+
+async function runTestCase(testCase: TestCase) {
+  if (testCase.status === "executing") return;
+  testCase.status = "executing";
+  await testCase
+    .test()
+    .then((result) => {
+      testCase.status = result ? "success" : "error";
+    })
+    .catch(() => {
+      testCase.status = "error";
+    });
+}
+
+export const Test = {
+  httpFetchCase,
+  tabHttpFetchCase,
+  runTestCase,
+};
